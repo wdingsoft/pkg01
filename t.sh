25b81717a480b6e74af7b30710c51f4515a378f4
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-07-12 00:48:24 -0400

    t.sh

---
+++ t.sh
@@ -0,0 +1,10 @@
+#/bin/sh 
+
+git rm    "src/pubref/ZhongYanYuan/New Rich Text Document \(4\).rtf"
+git rm    "src/pubref/images/yinxu-figures/imgs/ViewClick\(1\).htm"
+git rm    "src/pubref/images/yinxu-figures/imgs/ViewClick\(2\).htm"
+git rm    "src/pubref/images/yinxu-figures/imgs/ViewClick.htm"
+git rm    "src/pubref/images/yinxu-figures/imgs/onclick.htm"
+git rm    "src/pubref/kaogu/Clark Art Institute Reopens With New and Renovated Space - NYTimes.com_files/b5bcf2eba4"
+git rm    "src/pubref/kaogu/docin/JiaguZiYiXin.txt"
+git rm    "src/pubref/oracle123-old/calc/index03_allwordsForYa.php"
